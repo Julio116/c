72764e55d3a7f60f0ba987d4c1b772db5514da2e
Julio116 <juliobezerradelim@gmail.com>
2026-03-11 15:33:27 -0300

    fim exercicio 1

--- lista1/ex3.c
+++ lista1/ex3.c
@@ -5,12 +5,12 @@
 
 int main(void)
 {
-    int dinheiro_total = 0;
+    long long unsigned dinheiro_total = 0;
     double porcent_dinheiro1 = 0;
     double porcent_dinheiro2 = 0;
     double porcent_dinheiro3 = 0;
 
-    scanf(" %d %lf %lf %lf", &dinheiro_total, &porcent_dinheiro1,
+    scanf(" %llu %lf %lf %lf", &dinheiro_total, &porcent_dinheiro1,
         &porcent_dinheiro2, &porcent_dinheiro3);
 
 
@@ -18,11 +18,14 @@ int main(void)
     double premio2 = dinheiro_total * (porcent_dinheiro2 / 100);
     double premio3 = dinheiro_total * (porcent_dinheiro3 / 100);
 
-    
-    int dinheiro_rebeca = 3;
-    int invest_rebeca = 0;
 
-    int divisao_funcionou = (
+    long long unsigned soma_parcelas_3 = 0;
+
+    
+    long long unsigned dinheiro_rebeca = 3;
+    long long unsigned invest_rebeca = 0;
+
+    long long unsigned divisao_funcionou = (
             floor(premio1) == premio1 &&
             floor(premio2) == premio2 &&
             floor(premio3) == premio3
@@ -44,7 +47,7 @@ int main(void)
         double premio2 = dinheiro_total * (porcent_dinheiro2 / 100);
         double premio3 = dinheiro_total * (porcent_dinheiro3 / 100);
 
-        int divisao_funcionou = (
+        long long unsigned divisao_funcionou = (
             floor(premio1) == premio1 &&
             floor(premio2) == premio2 &&
             floor(premio3) == premio3
@@ -69,27 +72,27 @@ int main(void)
 
             scanf(" %c %c %c", &letra1, &letra2, &letra3);
 
-            int soma = 0;
-            int valor1 = letra1 - 96;
-            int valor2 = letra2 - 96;
-            int valor3 = letra3 - 96;
+            long long unsigned soma = 0;
+            long long unsigned valor1 = letra1 - 96;
+            long long unsigned valor2 = letra2 - 96;
+            long long unsigned valor3 = letra3 - 96;
 
             soma = valor1 + valor2 + valor3;
 
-            printf("%d\n", soma);
+            // printf("%llu\n", soma);
 
 
             double premio1 = dinheiro_total * (porcent_dinheiro1 / 100);
             double premio2 = dinheiro_total * (porcent_dinheiro2 / 100);
             double premio3 = dinheiro_total * (porcent_dinheiro3 / 100);
 
-            int divisao_funcionou = (
+            long long unsigned divisao_funcionou = (
                 floor(premio1) == premio1 &&
                 floor(premio2) == premio2 &&
                 floor(premio3) == premio3
             );
 
-            int sobra_int = dinheiro_total
+            long long unsigned sobra_int = dinheiro_total
                 - premio1 - premio2 - premio3;
             
             if ((divisao_funcionou == 1) && (sobra_int >= invest_rebeca + 1))
@@ -105,35 +108,34 @@ int main(void)
                 dinheiro_total += 1;
 
 
-                int idade1 = 0;
-                int idade2 = 0;
-                int idade3 = 0;
+                long long unsigned idade1 = 0;
+                long long unsigned idade2 = 0;
+                long long unsigned idade3 = 0;
 
-                scanf(" %d %d %d", &idade1, &idade2, &idade3);
+                scanf(" %llu %llu %llu", &idade1, &idade2, &idade3);
 
-                int soma_parcelas_3 = 0;
+                soma_parcelas_3 = 0;
 
-                if (idade1 / 3)
+                if ((idade1 % 3) == 0)
                     soma_parcelas_3 += idade1 / 3;
-                if (idade2 / 3)
+                if ((idade2 % 3) == 0)
                     soma_parcelas_3 += idade2 / 3;
-                if (idade3 / 3)
+                if ((idade3 % 3) == 0)
                     soma_parcelas_3 += idade3 / 3;
 
-                printf("%d\n", soma_parcelas_3);
 
 
                 double premio1 = dinheiro_total * (porcent_dinheiro1 / 100);
                 double premio2 = dinheiro_total * (porcent_dinheiro2 / 100);
                 double premio3 = dinheiro_total * (porcent_dinheiro3 / 100);
 
-                int divisao_funcionou = (
+                long long unsigned divisao_funcionou = (
                     floor(premio1) == premio1 &&
                     floor(premio2) == premio2 &&
                     floor(premio3) == premio3
                 );
 
-                int sobra_int = dinheiro_total
+                long long unsigned sobra_int = dinheiro_total
                     - premio1 - premio2 - premio3;
                 
                 if ((divisao_funcionou == 1) && (sobra_int >= invest_rebeca + 1))
@@ -148,6 +150,15 @@ int main(void)
                 }
             }
         }
+    }
+
+    if (invest_rebeca >= 2)
+    {
+        printf("%llu\n", dinheiro_total);
+    }
+    if (invest_rebeca >= 3)
+    {
+        printf("%llu\n", soma_parcelas_3);
     }
 
     if (dinheiro_rebeca >= 7)
---
+++ testes/teste.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+typedef struct {
+    double d;
+    int i;
+    char c[10];
+} widget;
+
+int main(void)
+{
+    return EXIT_SUCCESS;
+}
