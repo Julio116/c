8aa622962256cd48823b95421f87fa187539596d
Julio116 <juliobezerradelim@gmail.com>
2026-05-20 13:53:03 -0300

    fim

--- lista8/ex3.c
+++ lista8/ex3.c
@@ -1,3 +1,8 @@
+// INCOMPLETO
+// INCOMPLETO
+// INCOMPLETO
+// INCOMPLETO
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,7 +13,7 @@ const char nome_template[10] = "irisX.csv";
 
 typedef struct Flower Flower;
 typedef struct FlowerSet FlowerSet;
-typedef struct FlowerData FlowerData;
+typedef struct FlowerSetData FlowerSetData;
 typedef enum FlowerProp FlowerProp;
 
 
@@ -29,11 +34,12 @@ struct Flower {
 struct FlowerSet {
     size_t num_flowers;
     Flower** set;
+    FlowerSetData *flowerset_data;
 };
 
 
 // estatisticas relacionadas a uma flor
-struct FlowerData {
+struct FlowerSetData {
     double media_sepal_l;
     double media_sepal_w;
     
@@ -44,8 +50,9 @@ struct FlowerData {
 };
 
 
-// aloca e inicializa flor vazia
-Flower* alloc_flower() {
+// aloca e inicializa flor vazia.
+// a aloca acontece com `realloc()`.
+Flower* realloc_flower() {
     Flower *flower = NULL;
     Flower *tmp = realloc(flower, sizeof(Flower));
 
@@ -62,7 +69,8 @@ Flower* alloc_flower() {
 }
 
 
-// aloca e inicializa flowerset vazio
+// aloca e inicializa flowerset vazio.
+// retorna NULL em caso de erro.
 FlowerSet* alloc_flowerset() {
     FlowerSet *flowerset = malloc( sizeof(FlowerSet) );
 
@@ -96,62 +104,31 @@ char* get_filename(size_t index) {
 }
 
 
-// le os dados do arquivo e armazena em `flower`
-int read_line_csv(FILE *iris_csv, Flower *flower) {
-    int status_reading = fscanf(iris_csv,
-        " %lf,%lf,%lf,%lf,%99s",
-        &(flower->sepal_length),
-        &(flower->sepal_width),
-        &(flower->petal_length),
-        &(flower->petal_width),
-        flower->species
-    );
-
-    return status_reading;
-}
-
-
-void print_flower(Flower *flower, char *nome_arquivo) {
-    printf("Arquivo: %s\n",         nome_arquivo);
+void print_flower_caracteristicas(Flower *flower) {
+    printf("### CARACTERISTICAS\n");
     printf("Sepal Length: %lf\n",   flower->sepal_length);
     printf("Sepal Width: %lf\n",    flower->sepal_width);
     printf("Petal Length: %lf\n",   flower->petal_length);
     printf("Petal Width: %lf\n",    flower->petal_width);
     printf("Species: %s\n",         flower->species);
-    printf("\n");
 }
 
 
-// retorna 0 em caso de erro.
-int read_csvs(int num_arquivos) {
-    for (size_t i = 0; i < num_arquivos; i++)
-    {
-        char* nome_arquivo = get_filename(i);
-        FILE *iris_csv = NULL;
+void print_flower_estatisticas(FlowerSetData *flower_data) {
+    printf("### ESTATISTICAS");
+    printf("Sepal Length (media): %lf\n", flower_data->media_sepal_l);
+    printf("Sepal Width  (media): %lf\n", flower_data->media_sepal_w);
+    printf("Petal Length (media): %lf\n", flower_data->media_petal_l);
+    printf("Petal Width  (media): %lf\n", flower_data->media_petal_w);
+    printf("Species      (moda) : %d\n" , flower_data->moda_species);
+}
 
-        Flower* flower = alloc_flower();
-        if (flower == NULL) return 0;
 
-
-        if ( (iris_csv = fopen(nome_arquivo, "r")) == NULL) {
-            printf("Nao foi possivel abrir o arquivo %s\n", nome_arquivo);
-        } else {
-            int status_reading = read_line_csv(iris_csv, flower);
-            if (status_reading == EOF) {
-                printf("Nao foi possivel ler a linha do arquivo %s\n", nome_arquivo);
-            }
-
-            print_flower(flower, nome_arquivo);
-
-            if (feof(iris_csv)) {
-                free(flower);
-
-                int closing_status = fclose(iris_csv);
-                if (closing_status == EOF) return 0;
-            }
-        }
-    }
-    return 1;
+void print_flower(Flower *flower, FlowerSetData *flowerset_data, char *nome_arquivo) {
+    printf("Arquivo: %s\n", nome_arquivo);
+    print_flower_caracteristicas(flower);
+    print_flower_estatisticas(flowerset_data);
+    printf("\n");
 }
 
 
@@ -187,7 +164,7 @@ double* get_prop_flowers(FlowerSet *flowerset, FlowerProp PROP) {
 }
 
 
-double media_aritmetica(unsigned n, double *arr) {
+double media_aritmetica(size_t n, double *arr) {
     double soma = 0;
 
     for (size_t i = 0; i < n; i++)
@@ -201,7 +178,7 @@ double media_aritmetica(unsigned n, double *arr) {
 }
 
 
-void registrar_media(FlowerData *flower_data, double media, FlowerProp PROP) {
+void registrar_media(FlowerSetData *flower_data, double media, FlowerProp PROP) {
     switch (PROP) {
         case SEPAL_L:
             flower_data->media_sepal_l = media;
@@ -219,26 +196,82 @@ void registrar_media(FlowerData *flower_data, double media, FlowerProp PROP) {
 }
 
 
-FlowerData* get_flower_data(FlowerSet *flowerset) {
-    FlowerData *flower_data = malloc( sizeof(FlowerData) );
-    if (flower_data == NULL) return NULL;
-
-    size_t num_flowers = flowerset->num_flowers;
+// pega as estatisticas do `flowerset` inteiro, e coloca dentro de `flowerset`
+FlowerSetData* get_flower_data(FlowerSet *flowerset) {
+    FlowerSetData *flowerset_data = malloc( sizeof(FlowerSetData) );
+    if (flowerset_data == NULL) return NULL;
 
     // calcular as medias de todas as propriedades das flores
-    for (size_t PROP = SEPAL_W; PROP < PETAL_W; i++)
+    for (size_t PROP = SEPAL_W; PROP < PETAL_W; PROP++)
     {
         double* prop_arr = get_prop_flowers(flowerset, SEPAL_L);
         if (prop_arr == NULL) return NULL;
 
-        double media = media_aritmetica(num_flowers, PROP);
-
-        registrar_media(flower_data, media, PROP);
+        double media = media_aritmetica(flowerset->num_flowers, prop_arr);
+        registrar_media(flowerset_data, media, PROP);
     }
 
     // falta moda
+    return flowerset_data;
+}
 
-    return flower_data;
+
+// le os dados do arquivo `iris_csv`, e coloca os dados em `flowerset`
+int read_flowers_csv(FILE *iris_csv, FlowerSet *flowerset) {
+    int status_reading = 1;
+
+    do {
+        Flower *flower = realloc_flower();
+        if (flower == NULL) return 0;
+
+        int status_reading = fscanf(iris_csv,
+            " %lf,%lf,%lf,%lf,%99s",
+            &(flower->sepal_length),
+            &(flower->sepal_width),
+            &(flower->petal_length),
+            &(flower->petal_width),
+            flower->species
+        );
+
+        if (status_reading == EOF) return 0;
+
+        add_flower_to_set(flower, flowerset);
+    } while(status_reading != EOF);
+
+    FlowerSetData* flowerset_data = get_flower_data(flowerset);
+    flowerset->flowerset_data = flowerset_data;
+
+    return 1;
+}
+
+
+// retorna 0 em caso de erro.
+int read_csvs(int num_arquivos) {
+    for (size_t i = 0; i < num_arquivos; i++)
+    {
+        char* nome_arquivo = get_filename(i);
+        FILE *iris_csv = NULL;
+
+        if ( (iris_csv = fopen(nome_arquivo, "r")) == NULL) {
+            printf("Nao foi possivel abrir o arquivo %s\n", nome_arquivo);
+        } else {
+            FlowerSet *flowerset = alloc_flowerset();
+            if (flowerset == NULL) return 0;
+
+            int status_reading = read_flowers_csv(iris_csv, flowerset);
+
+            if (status_reading == 0) {
+                printf("Nao foi possivel ler a linha do arquivo %s\n", nome_arquivo);
+            }
+
+
+            if (feof(iris_csv)) {
+                int closing_status = fclose(iris_csv);
+                if (closing_status == EOF) return 0;
+            }
+        }
+    }
+    return 1;
 }
 
 
